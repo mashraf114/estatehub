de25ac725e4881df26be10c81cff5d2b88c9001f
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2026-04-29 12:52:16 +0300

    feat: add urls

---
+++ backend/config/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from properties.views import PropertyViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = DefaultRouter()
+router.register("properties", PropertyViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
+]
